7aba064d28865b72e0a9e00326f2d7c9fd6d2993
sdparkjr <sdparkjr@gmail.com>
2018-03-01 22:18:55 -0300

    Edit Home Countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { CountryProvider } from '../providers/country/country';
@@ -15,7 +17,8 @@ import { CountryProvider } from '../providers/country/country';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { CountryModel } from '../../models/country.model';
+import { FormControl } from '@angular/forms';
+import { CountryProvider } from '../../providers/country/country';
+
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +15,80 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  
+  listcountries: CountryModel[] = [];
+  //paises: PaisModel[] = [];
+  errorMessage: string;
+  load: any;
+  pesquisar: '';
+  pesquisarControl: FormControl;
+
+  constructor(public navCtrl: NavController,    
+    private _countryServ: CountryProvider,
+    private _loadingCtr: LoadingController,
+    private _alertCtrl: AlertController, ) {
+
+    this.pesquisarControl = new FormControl();
+  }
+
+  ionViewDidLoad() {
+
+      this.getAllCountries();
+
+    this.pesquisarControl.valueChanges.debounceTime(700).subscribe(search => {
+      console.log(search);
+      if (search !== '') {
+        this.getSearchCountry();
+      } else {
+        this.getAllCountries();
+      }
+    });
+  }
+
+  getAllCountries() {
+    
+    this.openLoad('Listando Paises');
+    this._countryServ.getCountries().subscribe(resultado => this.listcountries = resultado,
+      (error: Response | any) => {
+        //  console.log(error);
+        this.closeLoad();
+        let alert = this._alertCtrl.create({ message: `Ocorreu um erro:`, title: 'Falha', buttons: [{ text: 'Estou cinte!' }] });
+        alert.present();
+      },
+      () => {
+        //console.log("terminou"); 
+        this.closeLoad();
+      });
 
   }
 
+  getSearchCountry() {
+    
+    //pesquisar na lista que está online
+    let list = this.listcountries.filter(it => {
+      return it.name.toLowerCase().includes(this.pesquisar.toLowerCase()); //filtro com dados online
+    });
+
+    if (list.length > 0) {
+      this.listcountries = list;
+    } else {
+      //pesquisa na Api
+      this._countryServ.getSearch(this.pesquisar).subscribe(
+        resultado => this.listcountries = resultado, 
+        () => { }
+      );
+    }
+
+  }
+
+
+
+  ///////////Alert Modal/////////////////
+  openLoad(msg: string) {
+    this.load = this._loadingCtr.create({ content: msg });
+    this.load.present();
+  }
+  closeLoad() {
+    this.load.dismiss();
+  }
 }
